a7d40d85b8c04db7fee3f4a10af08b82ba3e577e
[Mary] <[brusnitsyna.maria@gmail.com]>
2024-02-25 11:18:53 +0500

    Добавили 4 задание в 1 урок

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 5;
+int b = 9;
+int c = 10;
+int d = 2;
+int e = 12;
+int max = a;
+if(b > max) {
+    max = b;
+} if(c > max) {
+    max = c;
+} if(d > max) {
+    max = d;
+} if(e > max) {
+    max = e;
+} 
+Console.WriteLine(max);
+ 
